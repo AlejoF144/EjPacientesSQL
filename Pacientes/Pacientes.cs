f62e7aa905a0f43cbff3602a4ff64e52ed3cf49b
AlejoF144 <164935968+AlejoF144@users.noreply.github.com>
2024-05-24 18:14:34 -0300

    Add files via upload

---
+++ Pacientes/Pacientes.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacientes
+{
+    internal class Pacientes
+    {
+        #region Propiedades y atributos
+        private int id;
+        private string nombre;
+        private string apellido;
+        private DateTime fechaNacimiento;
+        private int idSexo;
+        private int dni;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+        public DateTime FechaNacimiento
+        {
+            get { return fechaNacimiento; }
+            set { fechaNacimiento = value; }
+        }
+        public int IdSexo
+        {
+            get { return idSexo; }
+            set{ idSexo = value;}
+        }
+        public int Dni
+        {
+            get { return dni; }
+            set
+            {
+                dni = value;
+            }
+        }
+        #endregion
+
+        public Pacientes() { }
+
+        public Pacientes(string pNombre, string pApellido, DateTime pFechaNacimiento, int  pIdSexo,int pDni)
+        {
+            Nombre = pNombre;
+            Apellido = pApellido;
+            FechaNacimiento= pFechaNacimiento;
+            IdSexo = pIdSexo;
+            Dni = pDni;
+        }
+
+        public bool Nuevo()
+        {
+            bool correcto;
+            string consulta = $"INSERT INTO Pacientes (Nombre,Apellido,FechaNacimiento,IdSexo,Dni)" +
+                $" VALUES ('{Nombre}', '{Apellido}', '{FechaNacimiento}',{idSexo}, {Dni} ) ";
+            correcto = BaseDatos.EjecutarConsulta(consulta);
+            return correcto;
+        }
+        static public DataTable BuscarTodo()
+        {
+            DataTable dt = new DataTable();
+            string consulta = $"SELECT Pacientes.id AS IdPaciente, Pacientes.Apellido, Pacientes.FechaNacimiento, Pacientes.IdSexo, Pacientes.Dni, " +
+                $"Sexos.Id AS IdSexo, Sexos.Descripcion AS Sexo FROM Pacientes INNER JOIN Sexos ON Pacientes.IdSexo= Sexos.Id";
+            dt = BaseDatos.Buscar(consulta);
+            return dt;
+        }
+    }
+}
---
+++ Pacientes/frmPacientes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pacientes
+{
+    public partial class frmPacientes : Form
+    {
+        public frmPacientes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            frmNuevoPaciente frm = new frmNuevoPaciente();
+            frm.Show();
+        }
+
+        private void btnBuscarTodos_Click(object sender, EventArgs e)
+        {
+            DataTable dt = Pacientes.BuscarTodo();
+            LlenarGrilla(dt);
+        }
+        private void LlenarGrilla(DataTable dt)
+        {
+            dgvPacientes.DataSource = null;
+            dgvPacientes.DataSource = dt;
+        }
+
+        private void dgvPacientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
